f185a6ea35961d6228f67be080486ba47bcdfbf1
CYB <zero-bin@naver.com>
2024-08-16 10:10:15 +0900

    init

---
+++ src/main/java/com/cus/center/CustomerCenterApplication.java
@@ -0,0 +1,13 @@
+package com.cus.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cus/center/CustomerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cus.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
